ce4e3d35d51ab8f6810b0a43d7f573fbc8bd5021
SanDaGods <christianjedhsandagan@gmail.com>
2025-06-19 09:53:07 +0800

    Delete backend directory

--- backend/models/ApplicantCounter.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const applicantCounterSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  seq: { type: Number, default: 1000 }
-}, { collection: "ApplicantCounters" });
-
-module.exports = mongoose.model("ApplicantCounter", applicantCounterSchema);
